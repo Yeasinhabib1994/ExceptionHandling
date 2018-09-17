58066e4911599794dcda25dd51e659a8bb4937ea
yeasin habib <yeasinhabib1994@gmail.com>
2018-09-17 17:41:15 +0600

    Add project files.

---
+++ ExceptionHandling/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExceptionHandling
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                var calculator = new Calculator();
+                var result = calculator.Divide(5, 0);
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Sorry, an unexpected error occured");
+            }
+        }
+    }
+
+    public class Calculator
+    {
+        public int Divide(int a, int b)
+        {
+            return a / b;
+        }
+        public int Add(int a, int b)
+        {
+            return a + b;
+        }
+        public int Multiply(int a, int b)
+        {
+            return a * b;
+        }
+        public int Subtract(int a, int b)
+        {
+            return a - b;
+        }
+    }
+}
